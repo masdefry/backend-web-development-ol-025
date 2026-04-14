d979c690a13c5701a565961234ce18388e99b0d5
masdefry <muhammaddefryan@gmail.com>
2026-04-14 21:00:34 +0700

    auth register

--- blog-api/src/controllers/users.controller.ts
+++ blog-api/src/controllers/users.controller.ts
@@ -1,4 +1,20 @@
+import { Request, Response } from 'express';
+import { usersService } from '../services/users.service';
+
 export const usersController = {
   async login() {},
-  async register() {},
+  async register(req: Request, res: Response) {
+    const { email, password, fullName } = req?.body;
+
+    await usersService?.register({ email, password, fullName });
+
+    res.status(201).json({
+      success: true,
+      message: 'Register user successful',
+      data: {
+        email,
+        fullName,
+      },
+    });
+  },
 };
--- blog-api/src/services/users.service.ts
+++ blog-api/src/services/users.service.ts
@@ -1,4 +1,19 @@
+import { User } from '../../generated/prisma/client';
+import { prisma } from '../configs/prisma-client.config';
+
 export const usersService = {
   async login() {},
-  async register() {},
+  async register({
+    email,
+    password,
+    fullName,
+  }: Pick<User, 'fullName' | 'email' | 'password'>) {
+    await prisma.user.create({
+      data: {
+        email,
+        password,
+        fullName,
+      },
+    });
+  },
 };
