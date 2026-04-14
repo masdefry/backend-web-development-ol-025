6f8fb18d5f787199b6de48e27c3173def358fc78
masdefry <muhammaddefryan@gmail.com>
2026-04-14 21:18:52 +0700

    auth login

--- blog-api/src/controllers/users.controller.ts
+++ blog-api/src/controllers/users.controller.ts
@@ -2,7 +2,19 @@ import { Request, Response } from 'express';
 import { usersService } from '../services/users.service';
 
 export const usersController = {
-  async login() {},
+  async login(req: Request, res: Response) {
+    const { email, password } = req?.body;
+
+    const loginWithoutPassword = await usersService?.login({ email, password });
+
+    res.status(200).json({
+      success: true,
+      message: 'Authentication user successful',
+      data: {
+        ...loginWithoutPassword,
+      },
+    });
+  },
   async register(req: Request, res: Response) {
     const { email, password, fullName } = req?.body;
 
--- blog-api/src/services/users.service.ts
+++ blog-api/src/services/users.service.ts
@@ -2,7 +2,21 @@ import { User } from '../../generated/prisma/client';
 import { prisma } from '../configs/prisma-client.config';
 
 export const usersService = {
-  async login() {},
+  async login({ email, password }: Pick<User, 'email' | 'password'>) {
+    const findUserByEmailAndPassword = await prisma.user.findUnique({
+      where: {
+        email,
+        password,
+      },
+    });
+
+    if(!findUserByEmailAndPassword) throw new Error('Invalid credential user account');
+
+    return {
+      email: findUserByEmailAndPassword?.email, 
+      fullName: findUserByEmailAndPassword?.fullName
+    }
+  },
   async register({
     email,
     password,
